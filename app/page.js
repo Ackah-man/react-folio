ca19302ebad073e182743c67683db4a53092f759
@o5w1n <ohemanackah@gmail.com>
2025-02-13 09:17:22 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Navbar from "./components/Navbar";
-import Head from "./components/Head";
-import About from "./components/About";
+import Navbar from "./Components/Navbar";
+import Head from "./Components/Head";
+import About from "./Components/About";
 import Project from "./Components/Project";
-import Service from "./components/Service";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Service from "./Components/Service";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
